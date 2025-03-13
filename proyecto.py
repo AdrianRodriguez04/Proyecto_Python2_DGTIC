7c7d447e08d481f5d52020f8a80d4aa94b427eb8
AdrianRodriguez04 <adrianrodriguez34@aragon.unam.mx>
2025-03-12 22:57:03 -0600

    Se añadieron más clases y se hizo la primera prueba. NOTA: Hay algunos errores que se tienen que corregir

--- proyecto.py
+++ proyecto.py
@@ -63,4 +63,91 @@ class Cliente:
         self.mesaAsignada = None
         self.pedidoActual = Pedido()
 
+    def asignarMesa(self, mesa):
+        self.mesaAsignada = mesa
+
+    def realizarPedido (self, items):
+        for item in items:
+            self.pedidoActual.agregarItem(item)
+
+    def verCuenta(self):
+        total = self.pedidoActual.calcularTotal()
+        print(f"Total a pagar: ${total}")
+        return total
     
+class Restaurante:
+    def __init__(self):
+        self.mesas = []
+        self.menu = Menu()
+        self.clientes = []
+
+    def añadirMesa(self, numero, capacidad):
+        self.mesas.append(Mesa(numero, capacidad))
+
+    def eliminarMesa(self, numero):
+        self.mesas = [mesa for mesa in self.mesas if mesa.numero != numero]
+
+    def mostrarMesasDisponibles(self):
+        for mesa in self.mesas:
+            if mesa.verificarEstado() == 'Libre':
+                print(f"Mesa {mesa.numero} (Capacidad: {mesa.capacidad}) esta disponible.")
+
+    def hacerReservacion (self, cliente, numeroMesa):
+        for mesa in self.mesas:
+            if mesa.numero == numeroMesa and mesa.reservar():
+                cliente.asignarMesa(mesa)
+                self.clientes.append(cliente)
+                print(f"Mesa {numeroMesa} reservada para {cliente.nombre}")
+                return True
+        print(f"Mesa {numeroMesa} no esta disponible.")
+        return False
+    
+    def gestionarPedido(self, cliente, items):
+        if cliente in self.clientes:
+            cliente.realizarPedido(items)
+            print(f"Pedido realizado para {cliente.nombre}.")
+        else:
+            print("Cliente no registrado.")
+
+    def mostrarMenu(self):
+        self.menu.mostrarMenu()
+
+    def escogerItemsMenu(self,cliente):
+        if cliente in self.clientes:
+            self.mostrarMenu()
+            itemsSeleccionados = []
+            while True:
+                itemNombre = input("Ingrese el nombre del item que desea (o 'x' para terminar): ")
+                if itemNombre.lower() == 'x':
+                    break
+                itemEncontrado = next((item for item in self.menu.items if item.nombre.lower() == itemNombre), None)
+                if itemEncontrado:
+                    itemsSeleccionados.append(itemEncontrado)
+                else:
+                    print("Item no encontrado. Intente de nuevo.")
+            self.gestionarPedido(cliente, itemsSeleccionados)
+        else:
+            print("Cliente no registrado")
+
+    def mostrarCuenta(self, cliente):
+        if cliente in self.clientes:
+            cliente.verCuenta()
+        else:
+            print("Cliente no registrado")
+
+restaurante = Restaurante()
+restaurante.añadirMesa(1,4)
+restaurante.añadirMesa(2,2)
+restaurante.menu.agregarItem('Tacos','Tacos de pastor con todo',10.0)
+restaurante.menu.agregarItem('Torta','Torta de pastor con todo',50.0)
+restaurante.mostrarMesasDisponibles()
+
+cliente = Cliente('Adrián Rodríguez')
+cliente2 = Cliente("Pablo Molina")
+
+restaurante.hacerReservacion(cliente,1)
+
+restaurante.hacerReservacion(cliente2,2)
+
+restaurante.escogerItemsMenu(cliente)
+restaurante.mostrarCuenta(cliente)
