1ac1d8a7c1a5ef6164866c9e2a36f20cd1b31e8f
AdrianRodriguez04 <adrianrodriguez34@aragon.unam.mx>
2025-03-14 21:43:21 -0600

    Cambie la base de datos eliminando una tabla y cambiando al usuario root

--- base.sql
+++ base.sql
@@ -1,59 +1,29 @@
-**-------------------------------------------
-CREATE DATABASE restaurante;
+-------------------------------------------
+CREATE DATABASE proyecto;
 
-CREATE USER 'empleado'@'localhost' IDENTIFIED BY 'contraseñaPropia';
-
-GRANT ALL PRIVILEGES ON restaurante.* TO 'empleado'@'localhost';
-FLUSH PRIVILEGES;
-
-EXIT;
-
-**-------------------------------------------
-USE restaurante;
+-------------------------------------------
+USE proyecto;
 
 CREATE TABLE mesas (
-id INT AUTO_INCREMENT PRIMARY KEY,
-numero INT NOT NULL,
+numero INT PRIMARY KEY,
 capacidad INT NOT NULL,
-estado VARCHAR(20) DEFAULT 'libre'
+estado ENUM('libre', 'ocupada') DEFAULT 'libre'
 );
 
-CREATE TABLE menu(
+CREATE TABLE menu (
 id INT AUTO_INCREMENT PRIMARY KEY,
 nombre VARCHAR(100) NOT NULL,
 descripcion TEXT,
 precio DECIMAL(10,2) NOT NULL
 );
 
-CREATE TABLE clientes(
+CREATE TABLE clientes (
 id INT AUTO_INCREMENT PRIMARY KEY,
 nombre VARCHAR(100) NOT NULL,
 mesaAsignada INT,
-FOREIGN KEY (mesaAsignada) REFERENCES mesas(id)
-);
-
-CREATE TABLE pedidos (
-id INT AUTO_INCREMENT PRIMARY KEY,
-clienteId INT,
-estado VARCHAR(20) DEFAULT 'preparando',
-FOREIGN KEY (clienteId) REFERENCES clientes(id)
-);
-
-CREATE TABLE itemsPedido(
-id INT AUTO_INCREMENT PRIMARY KEY,
-pedidoId INT,
-itemId INT,
-FOREIGN KEY (pedidoId) REFERENCES pedidos(id),
-FOREIGN KEY (itemId) REFERENCES menu(id)
+FOREIGN KEY (mesaAsignada) REFERENCES mesas(numero)
 );
 
 SHOW tables;
 
 EXIT;
-
-
-
-
-
-
-
