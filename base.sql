a9fb0d1d3bf4412cdadc9d938d3525375436442b
AdrianRodriguez04 <adrianrodriguez34@aragon.unam.mx>
2025-03-16 19:11:22 -0600

    Modifique mi base de datos porque me di cuenta que no todas las tablas estaban relacionadas, ahora si lo están

--- base.sql
+++ base.sql
@@ -10,6 +10,13 @@ capacidad INT NOT NULL,
 estado ENUM('libre', 'ocupada') DEFAULT 'libre'
 );
 
+CREATE TABLE clientes (
+id INT AUTO_INCREMENT PRIMARY KEY,
+nombre VARCHAR(100) NOT NULL,
+mesaAsignada INT,
+FOREIGN KEY (mesaAsignada) REFERENCES mesas(numero)
+);
+
 CREATE TABLE menu (
 id INT AUTO_INCREMENT PRIMARY KEY,
 nombre VARCHAR(100) NOT NULL,
@@ -17,12 +24,26 @@ descripcion TEXT,
 precio DECIMAL(10,2) NOT NULL
 );
 
-CREATE TABLE clientes (
-id INT AUTO_INCREMENT PRIMARY KEY,
-nombre VARCHAR(100) NOT NULL,
-mesaAsignada INT,
-FOREIGN KEY (mesaAsignada) REFERENCES mesas(numero)
+CREATE TABLE pedidos (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    clienteId INT NOT NULL,
+    fecha TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    estado ENUM('preparando','listo','entregado') DEFAULT 'preparando',
+    FOREIGN KEY (clienteId) REFERENCES clientes(id)
 );
+
+CREATE TABLE detallePedidos (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    pedidoId INT,
+    itemId INT DEFAULT 1,
+    FOREIGN KEY (pedidoId) REFERENCES pedidos(id),
+    FOREIGN KEY (itemId) REFERENCES menu(id)
+);
+
+
+
+
+
 
 SHOW tables;
 
